7cba77acb3a946389907a73a232412774aae3c77
Abdul Aziz Senior Fall <sfallobertys@hotmail.com>
2020-02-22 21:02:21 +0000

    enregistrement et mise à jour du workflow, mise en forme du tableau d'affichage 22/02/2020 à 21h 02

--- WorkFlowDynamic/Models/SchemeStepFlowModel.cs
+++ WorkFlowDynamic/Models/SchemeStepFlowModel.cs
@@ -12,13 +12,14 @@ namespace WorkFlowDynamic.Models
         [Display(Name = "Nom de l'activité")]
         public string Activity { get; set; }
         [Display(Name = "Ordre Exécution")]
-        public int Ordre { get; set; }
+        public string Ordre { get; set; }
         [Display(Name = "Nombre Occurence")]
         public int Occurence { get; set; }
         [Display(Name = "Gestionnaire de service")]
         public string Gestionnaire { get; set; }
         [Display(Name = "Service")]
         public string Service { get; set; }
-        public Scheme_StepSet scheme_Step { get; set; }
+        public string step { get; set; }
+        public string id { get; set; }
     }
 }
