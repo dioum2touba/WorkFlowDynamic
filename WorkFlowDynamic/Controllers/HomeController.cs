7ffca5e9d2cf8aa5bb18dbe7d7db675ca86f12f5
Abdul Aziz Senior Fall <sfallobertys@hotmail.com>
2020-02-23 16:11:00 +0000

    Mise en place du démarrage du workflow 23/02/2020 à 16h 10

--- WorkFlowDynamic/Controllers/HomeController.cs
+++ WorkFlowDynamic/Controllers/HomeController.cs
@@ -4,6 +4,7 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 using WorkFlowDynamic.Models;
 
@@ -12,15 +13,21 @@ namespace WorkFlowDynamic.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly WorkflowdynamicContext _context;
+        private readonly WorkFlowServices _serviceFlow = new WorkFlowServices();
+        private List<ControleurModels> controleurs = null;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, WorkflowdynamicContext context)
         {
             _logger = logger;
+            _context = context;
+            controleurs = new List<ControleurModels>();
+            controleurs = _serviceFlow.CopyMasterData();
         }
 
         public IActionResult Index()
         {
-            return View();
+            return View(_context.SchemeWorkFlowSet.ToList());
         }
 
         public IActionResult Privacy()
@@ -28,10 +35,36 @@ namespace WorkFlowDynamic.Controllers
             return View();
         }
 
+        public IActionResult DemarrerWorkflow(long? id)
+        {
+            var workflow = _serviceFlow.LoadWorkFlow(controleurs, id);
+            HttpContext.Session.Set<List<SchemeStepFlowModel>>("WorkFlow", workflow);
+            HttpContext.Session.Set<string>("StepIdentifier", "0");
+            return LoadNextStep(HttpContext.Session.Get<List<SchemeStepFlowModel>>("WorkFlow"), Convert.ToInt32(HttpContext.Session.Get<string>("StepIdentifier")));
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+        public RedirectToActionResult LoadNextStep(List<SchemeStepFlowModel> workflowName, int stepIdentifier)
+        {
+            var defaultAction = workflowName.FirstOrDefault(w => w.Ordre == stepIdentifier.ToString());
+            //If the page is in a workflow
+            if (workflowName!= null && workflowName.Count>0)
+            {
+                 SchemeStepFlowModel step = _serviceFlow.GetNextStep(workflowName, stepIdentifier);
+               // return LoadNextStep(workflowName, Convert.ToInt32(stepIdentifier));
+                return RedirectToAction(step.Service, step.Gestionnaire);
+            }
+            else//if not
+            {
+                return RedirectToAction(defaultAction.Service, defaultAction.Gestionnaire);
+            }
+        }
+
+        
     }
 }
--- WorkFlowDynamic/Models/SchemeStepFlowModel.cs
+++ WorkFlowDynamic/Models/SchemeStepFlowModel.cs
@@ -9,18 +9,20 @@ namespace WorkFlowDynamic.Models
 {
     public class SchemeStepFlowModel
     {
-        [Display(Name = "Nom de l'activité")]
         public string Activity { get; set; }
-        [Display(Name = "Ordre Exécution")]
+
         public string Ordre { get; set; }
-        [Display(Name = "Nombre Occurence")]
+
         public int Occurence { get; set; }
-        [Display(Name = "Gestionnaire de service")]
+
         public string Gestionnaire { get; set; }
-        [Display(Name = "Service")]
+
         public string Service { get; set; }
+
         public string id { get; set; }
+
         public string Description { get; set; }
+
         public string DetailsControleurs { get; set; }
     }
 }
